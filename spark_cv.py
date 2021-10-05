446343f9b45107075f67c278a3164e3df144dbc0
Canis Ignem <jonperezetxebarria@gmail.com>
2021-10-05 15:24:13 +0200

    Update spark_cv.py

--- spark_cv.py
+++ spark_cv.py
@@ -12,8 +12,8 @@ from pyspark.ml.feature import VectorAssembler
 
 if __name__ == "__main__":
     conf = SparkConf(True)
-    #conf.set("spark.executor.memory", "8g")
-    #conf.set('spark.executor.cores', '2')
+    conf.set("spark.executor.memory", "32g")
+    conf.set('spark.executor.cores', '4')
 
     sc = SparkContext(
         appName="multilayer_perceptron_classification_example",
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     ovr = OneVsRest(classifier=lr)
 
     print("---Training---")
-    model = lr.fit(train)
+    model = ovr.fit(train)
 
     
     print("---Testing---")
