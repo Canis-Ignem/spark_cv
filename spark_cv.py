db84aa4c55a9f5550042dd79f4804232356739d8
Canis Ignem <jonperezetxebarria@gmail.com>
2021-10-05 14:20:01 +0200

    Update spark_cv.py

--- spark_cv.py
+++ spark_cv.py
@@ -2,6 +2,6 @@ spark.sparkContext.uiWebUrl
 df_training = (spark
                .read
                .options(header = False, inferSchema = True)
-               .csv("data/MNIST/mnist_train.csv"))
+               .csv("/user/keystone/mnist_data/train.csv"))
 
 df_training.count()
