cf63984b670d23b92e2b45082a05240604dff572
Canis Ignem <jonperezetxebarria@gmail.com>
2021-10-05 13:06:32 +0200

    image schema

--- spark_cv.py
+++ spark_cv.py
@@ -1,20 +1,21 @@
 from pyspark.sql import SparkSession
-from pyspark.ml.image import ImageSchema
 from pyspark.sql.functions import lit
 from functools import reduce
 # create a spark session
 spark = SparkSession.builder.appName('DigitRecog').getOrCreate()
 # loaded image
-zero = ImageSchema.readImages("0").withColumn("label", lit(0))
-one = ImageSchema.readImages("1").withColumn("label", lit(1))
-two = ImageSchema.readImages("2").withColumn("label", lit(2))
-three = ImageSchema.readImages("3").withColumn("label", lit(3))
-four = ImageSchema.readImages("4").withColumn("label", lit(4))
-five = ImageSchema.readImages("5").withColumn("label", lit(5))
-six = ImageSchema.readImages("6").withColumn("label", lit(6))
-seven = ImageSchema.readImages("7").withColumn("label", lit(7))
-eight = ImageSchema.readImages("8").withColumn("label", lit(8))
-nine = ImageSchema.readImages("9").withColumn("label", lit(9))
+zero = spark.read.format("image").load("mnist_data/0")
+one = spark.read.format("image").load("mnist_data/1")
+two = spark.read.format("image").load("mnist_data/2")
+three = spark.read.format("image").load("mnist_data/3")
+four = spark.read.format("image").load("mnist_data/4")
+five = spark.read.format("image").load("mnist_data/5")
+six = spark.read.format("image").load("mnist_data/6")
+seven = spark.read.format("image").load("mnist_data/7")
+eight = spark.read.format("image").load("mnist_data/8")
+nine = spark.read.format("image").load("mnist_data/9")
+
+print(zero.printSchema())
 dataframes = [zero, one, two, three,four,
              five, six, seven, eight, nine]
 # merge data frame
@@ -23,3 +24,5 @@ df = reduce(lambda first, second: first.union(second), dataframes)
 df = df.repartition(200)
 # split the data-frame
 train, test = df.randomSplit([0.8, 0.2], 42)
+
+print(train.show(5))
